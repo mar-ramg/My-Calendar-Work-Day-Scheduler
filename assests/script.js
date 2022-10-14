bc25148ff4eea607ad05c017b866d5c7974880ef
who cares <mariaaramirez80@gmail.com>
2022-10-14 09:27:15 -0700

    Added SaveBtn, a loop for the time blocks, and the columns to change the bg color

--- assests/script.js
+++ assests/script.js
@@ -1,6 +1,37 @@
 
-// Curent Day when you open the application at the top//
+// Curent Day when you open the application at the top //
 var curentDate = moment().format('dddd, MMMM Do YYYY')
 $("#currentDay").text(curentDate);
 
-// Presented Time Blocks //
+// Save Button //
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time,text);
+})
+
+// # of hours //
+function timeTracker() {
+    var timeNow = moment().hour();
+    // loop over time blocks //    
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    // To check time and add the classes for background to change color
+    if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+    } else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+    }
+
+    })
+}
